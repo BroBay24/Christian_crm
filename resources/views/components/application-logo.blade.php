36d988fc1f677c77fae88ecc8e22acab91464d6d
Bayfr <bayufrdinand@gmail.com>
2025-12-17 23:45:45 +0700

    kostumisasi tampilan dashboard login

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <!-- Atau jika di dalam tautan (link) -->
-<a href="{{ url('/') }}" class="text-xl text-gray-800" style="font-family: 'DM Sans', sans-serif; font-weight: 600;">
-    PT. Smart Internet Service Provider
+<a href="{{ url('/dashboard') }}" class="text-xl text-Blue-700" style="font-family: 'DM Sans', sans-serif; font-weight: 600;">
+    PT. Smart
 </a>
